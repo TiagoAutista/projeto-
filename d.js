102e4317e4ec24b9bbfefd28f963f3e5b97c8509
TiagoAutista <autistgonnaup@outlook.com>
2026-06-06 14:58:42 -0300

    Implement abrirPainelTipificacao function
    Added function to locate and click Tipificar/Edit button.

--- d.js
+++ d.js
@@ -1,1 +1,27 @@
+// src/lib/gps-next/subroutine/process/abrirPainelTipificacao.js
+const { aguardarPrimeFaces } = require("../../../../utils/helpers");
 
+/**
+ * Tenta localizar e clicar no botão de Tipificar/Editar para abrir o painel
+ */
+async function abrirPainelTipificacao(page) {
+  const botoes = [
+    "[id='formDadosClienteAtendimento:btnTipificar']",
+    "[id='formDadosClienteAtendimento:btnEditar']",
+    '[id*="tipificacao"] button',
+    '[data-testid="btn-tipificar"]',
+  ];
+
+  for (const sel of botoes) {
+    try {
+      const el = await page.$(sel);
+      if (el && (await el.isVisible())) {
+        await el.click();
+        await aguardarPrimeFaces(page, 5000);
+        return;
+      }
+    } catch { /* Tenta o próximo seletor */ }
+  }
+}
+
+module.exports = { abrirPainelTipificacao };
