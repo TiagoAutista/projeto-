a241d99e796e3088a32a9d9c49e88f96cbd12f5c
TiagoAutista <autistgonnaup@outlook.com>
2026-06-08 18:22:38 -0300

    Update a.js

--- a.js
+++ a.js
@@ -9,3 +9,5 @@
    📊 Extraindo informações de bloqueio...
    ⚠️ Nenhuma informação de bloqueio encontrada
    ✅ 0 informação(ões) de bloqueio extraída(s)
+
+Para poder fazer os testes corretamento quero que um tempo um pouco maior para que o sistema possa pegar as informações e que para seguir para proxima ordem o sistema peça para o usuario validar se quer continuar ou encerrar o processo
