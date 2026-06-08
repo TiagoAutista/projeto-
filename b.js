b46b1677c9c51b3b20b7bcb1e04d88142b79b820
TiagoAutista <autistgonnaup@outlook.com>
2026-06-08 16:28:00 -0300

    Add helper functions for waiting and user input

--- b.js
+++ b.js
@@ -1,1 +1,15 @@
+// src/lib/helpers.js
+const readline = require('readline');
 
+function aguardar(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function aguardarEnter(mensagem) {
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+  return new Promise(resolve => {
+    rl.question(mensagem, () => { rl.close(); resolve(); });
+  });
+}
+
+module.exports = { aguardar, aguardarEnter };
