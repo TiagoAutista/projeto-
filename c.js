4d03d20e5f46c72297b8a12469de093c84bfc75a
TiagoAutista <autistgonnaup@outlook.com>
2026-06-08 16:28:29 -0300

    Add SDU configuration section to config.js
    Added new SDU section with URL and selectors to config.

--- c.js
+++ c.js
@@ -1,1 +1,21 @@
+// src/config/config.js
+// Adicione isso ao objeto config existente:
 
+const config = {
+  // ... (suas configs existentes de wfm, gps, siebel)
+  
+  // ✅ NOVA SEÇÃO SDU
+  sdu: {
+    url: 'https://sdu.redecorp.br/DiagnoseServiceProblem/home',
+    selectors: {
+      homeState: '.ui-home-state',
+      searchInput: 'input.mat-input-element[formcontrolname="search"]',
+      searchButton: '.ui-button-home',
+      resultTable: '.mat-table',
+      resultCard: '.ui-card',
+      errorMessage: '.mat-error, .mat-snack-bar-container'
+    }
+  }
+};
+
+module.exports = config;
