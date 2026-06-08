c3c2755f20077b4600a3f92ab04046d6127ab6ef
TiagoAutista <autistgonnaup@outlook.com>
2026-06-08 13:13:43 -0300

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,1 @@
-⏳ Aguardando a interface carregar...
-✅ Interface pronta!
 
-🖱️ Abrindo o menu de seleção...
-
-❌ Erro durante a execução: page.waitForTimeout is not a function
-📸 Screenshot de erro salvo: erro_tela.png
-
-🔒 Fechando navegador...
-✅ Robô finalizado.
-PS C:\Users\A0161921\Desktop\auto-robo-main> 
