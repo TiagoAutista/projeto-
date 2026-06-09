210365c21d23d03f1385600de3baf53b993446dc
TiagoAutista <autistgonnaup@outlook.com>
2026-06-09 14:03:00 -0300

    Update c.js

--- c.js
+++ c.js
@@ -1,1 +1,2 @@
 
+Warning: Don’t paste code into the DevTools Console that you don’t understand or haven’t reviewed yourself. This could allow attackers to steal your identity or take control of your computer. Please type ‘allow pasting’ below and press Enter to allow pasting.
