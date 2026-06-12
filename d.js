e87a22af7ffedffebde016eb047da416d5f96c77
TiagoAutista <autistgonnaup@outlook.com>
2026-06-12 17:38:25 -0300

    Export utility functions from multiple modules

--- d.js
+++ d.js
@@ -1,1 +1,10 @@
+// utils/index.js - Exporta todas as funções utilitárias
+const terminal = require('./terminal');
+const visual = require('./visual');
+const validacao = require('./validacao');
 
+module.exports = {
+  ...terminal,
+  ...visual,
+  ...validacao
+};
