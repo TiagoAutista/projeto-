578fcddf28b986ff0b9d9e171de4923b2191b273
TiagoAutista <autistgonnaup@outlook.com>
2026-06-13 13:42:25 -0300

    Update j.js

--- j.js
+++ j.js
@@ -1,1 +1,5 @@
 const { chromium } = require('playwright');
+
+http://appwfm.gvt.net.br/wfm-search/detalhesWorkOrder.xhtml?wo=7613204754
+
+src/lib/wfm/wfm-login.js crie um robo que acesso e login no link acima.
