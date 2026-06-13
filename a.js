c30f4b70cbe57c323120cca9006ed8bf828d9b05
TiagoAutista <autistgonnaup@outlook.com>
2026-06-13 10:41:25 -0300

    Update a.js

--- a.js
+++ a.js
@@ -1,48 +1,72 @@
-// utils/visual.js - Funções de formatação visual com Chalk
-const chalk = require('chalk');
-const { limparTela } = require('./terminal');
+LINKS
+	
+Nomes
 
-const exibirCabecalho = (titulo) => {
-  limparTela();
-  const borda = chalk.cyan.bold;
-  const texto = chalk.white.bold;
-  const data = chalk.gray;
-  
-  console.log(borda('╔══════════════════════════════════════════════════════════╗'));
-  console.log(borda('║') + '   ' + texto(titulo.padEnd(54)) + borda('║'));
-  console.log(borda('║') + '   📅 ' + data(new Date().toLocaleString('pt-BR').padEnd(48)) + borda('║'));
-  console.log(borda('╚══════════════════════════════════════════════════════════╝\n'));
-};
 
-const desenharBox = (titulo, linhas) => {
-  const largura = 60;
-  const borda = chalk.cyan.bold;
-  const destaque = chalk.yellow.bold;
-  
-  console.log(borda('╔' + '═'.repeat(largura) + '╗'));
-  console.log(borda('║') + destaque(' ' + titulo.padEnd(largura - 1) + '║'));
-  console.log(borda('╠' + '═'.repeat(largura) + '╣'));
-  
-  linhas.forEach(linha => {
-    // Garante que o texto não ultrapasse a borda, mesmo com cores
-    const textoLimpo = linha.replace(/\x1b\[[0-9;]*m/g, ''); // Remove códigos de cor para calcular tamanho
-    const espacos = ' '.repeat(largura - 2 - textoLimpo.length);
-    console.log(borda('║') + ' ' + linha + espacos + borda('║'));
-  });
-  
-  console.log(borda('╚' + '═'.repeat(largura) + '╝'));
-};
+https://gvt.fs.ocs.oraclecloud.com/
+	
+Zeus
 
-const getStatusFormatado = (label, statusNavegador, statusLogin) => {
-  let indicador = chalk.red.bold('🔴 FECHADO');
-  if (statusNavegador) {
-    indicador = statusLogin ? chalk.green.bold('🟢 LOGADO') : chalk.yellow.bold('🟡 ABERTO (Sem Login)');
-  }
-  return `${chalk.white.bold(label.padEnd(8))}: ${indicador}`;
-};
 
-module.exports = {
-  desenharBox,
-  exibirCabecalho,
-  getStatusFormatado
-};
+http://appwfm.gvt.net.br/wfm-search/login.xhtml
+	
+WFM
+
+
+https://vivowfm.redecorp.br/wfm/login?logout=true
+	
+NICE
+
+
+https://goi.vivo.com.br/usuario/login.php
+	
+GOI
+
+
+http://192.168.236.167:8080/thinkcat/login.iface?login_error=true
+	
+DM
+
+
+https://sdu.redecorp.br/DiagnoseServiceProblem/signIn
+	
+SDU
+
+
+http://wisetool.gvt.net.br/wisetool/login.jsp
+	
+WISETOO
+
+
+https://auth.magictool.com.br/realms/magictool/protocol/openid-connect/auth?client_id=b2c-web&redirect_uri=https%3A%2F%2Fb2c.magictool.com.br%2F%3Fsession_state%3D41815b9c-f007-4d37-b467-9bcf38a157f0%26code%3D1000cbe6-503c-4166-9114-2a0fb8f0cbbe.41815b9c-f007-4d37-b467-9bcf38a157f0.b051ea77-c268-4e79-a6c0-ffdb4795eb30&state=e098b805-d0d4-499d-8a79-1b4b53c23cd6&response_mode=fragment&response_type=code&scope=openid&nonce=466eb0b3-b5e6-4c38-a09c-1968b6a9ba3e
+	
+MAGICTOO
+
+
+http://sagreosp.telefonica.br/cpqd/caweb/login.xhtml
+	
+SAGRES
+
+
+file://10.128.222.232/esse_spc$/Intranet/Home/INTRANET_VILA_MARIANA.html
+	
+SAFIRA
+
+
+http://wdein.redecorp.br:8080/InteractionWorkspaceIN/publish.htm
+	
+WDE
+
+
+http://gpscrm.gvt.com.br/gps/crm/atendimento/index.jsf
+	
+SIELBEL
+
+
+http://gps.redecorp.br/gps/atendimento/perfil.jsf
+	
+NEXT
+
+
+https://autenticaint.vivo.com.br/LoginCorp/?bmctx=981B69D7827342189291B68C92BD5A2E4B1A5842FAAEE8960AD24E2CEE9F7D79&contextType=external&username=string&challenge_topaz=true&challenge_url=https%3A%2F%2Fautenticaint.vivo.com.br%2FLoginCorp%2F&password=secure_string&request_id=-3477954222941187585&authn_try_count=0&locale=pt_BR&resource_url=%252Fuser%252Floginsso#Shell-home
+	 
